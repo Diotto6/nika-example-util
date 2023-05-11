68165485256c7c79a4e7bff469ca15c74cc4de8a
DiottoDev <nicodiottodev@gmail.com>
2023-05-11 09:06:16 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
 import { Button } from "nika-styles";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button border="2" color="gray">
-        Hello World!
-      </Button>
+    <main className="flex flex-col items-center justify-center gap-4 pt-16">
+      <p>
+        Examples of already styled button react component from lib nika styles
+      </p>
+      <div className="flex flex-col items-center justify-center gap-4 p-4">
+        <Button border={"1"} color={"blue"} size={"large"}>
+          Hello World!
+        </Button>
+
+        <Button border={"1"} color={"red"} size={"small"}>
+          Hello World!
+        </Button>
+      </div>
     </main>
   );
 }
